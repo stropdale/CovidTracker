7f24c55430f6405fccdaf2a355505cadf2629a99
Richard Stockdale <richardstockdale@mac.com>
2020-10-07 15:36:55 +0100

    Search

--- CovidTraker/CovidTraker/LocalAuthoritiesListViewController.swift
+++ CovidTraker/CovidTraker/LocalAuthoritiesListViewController.swift
@@ -9,62 +9,33 @@ import UIKit
 
 class LocalAuthoritiesListViewController: UIViewController {
 
+    let vm = LocalAuthoritiesListViewModel()
     
     @IBOutlet weak var segmentedController: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
-    
-    var dataModels: [LocalAuthorityModel]?
-    
-    var sortedDataModels: [LocalAuthorityModel]? {
-        get {
-            return segmentedController.selectedSegmentIndex == 0 ? alphaSortedDataModels : rankSortedDataModels
-        }
-    }
-    
-    var alphaSortedDataModels: [LocalAuthorityModel]? {
-        guard let models = dataModels else {
-            return nil
-        }
-        
-        return models.sorted(by: { (first, second) -> Bool in
-            return first.localAuthorityName < second.localAuthorityName
-        })
-    }
-    
-    var rankSortedDataModels: [LocalAuthorityModel]? {
-        guard let models = dataModels else {
-            return nil
-        }
-        
-        return models.sorted(by: { (first, second) -> Bool in
-            return first.rate.last! > second.rate.last!
-        })
-    }
-    
-    func getRankFor(model: LocalAuthorityModel) -> Int? {
-        guard let arr = rankSortedDataModels else {
-            return nil
-        }
-        
-        let name = model.localAuthorityName
-        for (i, m) in arr.enumerated() {
-            if name == m.localAuthorityName {
-                return i + 1
-            }
-        }
-        
-        return nil
-    }
+    @IBOutlet weak var searchBar: UISearchBar!
     
     @IBAction func segmentedControlChanged(_ sender: Any) {
         tableView.reloadData()
     }
     
+    var models: [LocalAuthorityModel] {
+        let models = vm.sortedAndFilteredDataModels(selectedIndex: segmentedController.selectedSegmentIndex,
+                                                          searchTerm: searchBar.text)
+        
+        return models
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        dataModels = DataLoader.loadBundledData()
+    }
+}
+
+// Search
+extension LocalAuthoritiesListViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        tableView.reloadData()
     }
 }
 
@@ -72,21 +43,24 @@ extension LocalAuthoritiesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 72
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let model = models[indexPath.row]
+        
+    }
 }
 
 extension LocalAuthoritiesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let models = dataModels else {
-            return 0
-        }
-        
         return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SummaryModelTableViewCell
-        let model = sortedDataModels![indexPath.row]
-        cell.populate(model: model, rank: getRankFor(model: model))
+        let model = models[indexPath.row]
+        cell.populate(model: model, rank: vm.getRankFor(model: model))
         
         return cell
     }
---
+++ CovidTraker/CovidTraker/LocalAuthoritiesListViewModel.swift
@@ -0,0 +1,79 @@
+//
+//  LocalAuthoritiesListViewModel.swift
+//  CovidTraker
+//
+//  Created by Richard Stockdale on 07/10/2020.
+//
+
+import Foundation
+
+class LocalAuthoritiesListViewModel {
+    
+    private var dataModels: [LocalAuthorityModel]?
+    
+    init() {
+        dataModels = DataLoader.loadBundledData()
+    }
+    
+    func sortedAndFilteredDataModels(selectedIndex: Int, searchTerm: String?) -> [LocalAuthorityModel] {
+        guard let models = selectedIndex == 0 ? alphaSortedDataModels : rankSortedDataModels else {
+            return [LocalAuthorityModel]()
+        }
+        
+        guard let searchTerm = searchTerm else {
+            return models
+        }
+        
+        if searchTerm.isEmpty {
+            return models
+        }
+        
+        return searchModels(models: models, term: searchTerm)
+    }
+    
+    private func searchModels(models: [LocalAuthorityModel], term: String) -> [LocalAuthorityModel] {
+        var filtered = [LocalAuthorityModel]()
+        for model in models {
+            if model.localAuthorityName.contains(term) {
+                filtered.append(model)
+            }
+        }
+        
+        return filtered
+    }
+    
+    private var alphaSortedDataModels: [LocalAuthorityModel]? {
+        guard let models = dataModels else {
+            return nil
+        }
+        
+        return models.sorted(by: { (first, second) -> Bool in
+            return first.localAuthorityName < second.localAuthorityName
+        })
+    }
+    
+    private var rankSortedDataModels: [LocalAuthorityModel]? {
+        guard let models = dataModels else {
+            return nil
+        }
+        
+        return models.sorted(by: { (first, second) -> Bool in
+            return first.rate.last! > second.rate.last!
+        })
+    }
+    
+    func getRankFor(model: LocalAuthorityModel) -> Int? {
+        guard let arr = rankSortedDataModels else {
+            return nil
+        }
+        
+        let name = model.localAuthorityName
+        for (i, m) in arr.enumerated() {
+            if name == m.localAuthorityName {
+                return i + 1
+            }
+        }
+        
+        return nil
+    }
+}
